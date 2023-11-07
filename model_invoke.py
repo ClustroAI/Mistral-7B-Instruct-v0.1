276b716fde8ee80fb0fb7d4b060f95dd13bd9e12
Yingqiang Liu <53621227+Remilistrasza@users.noreply.github.com>
2023-11-07 12:14:49 -0700

    Update model input/output

--- model_invoke.py
+++ model_invoke.py
@@ -30,16 +30,24 @@ def invoke(input_text):
     # generated_ids = model.generate(model_inputs, max_new_tokens=1000, do_sample=True)
     # decoded = tokenizer.batch_decode(generated_ids)
     # return decoded[0]
-    prompt = "Tell me about AI"
+  
+    input_json = json.loads(input_text)
+    prompt = input_json['prompt']
     prompt_template=f'''<s>[INST] {prompt} [/INST]
     '''
     
+    # Generator configs
+    temperature = input_json['temperature'] if 'temperature' in input_json else 0.7
+    top_p = input_json['top_p'] if 'top_p' in input_json else 0.95
+    top_k = input_json['top_k'] if 'top_k' in input_json else 40
+    max_new_tokens = input_json['max_new_tokens'] if 'max_new_tokens' in input_json else 512
+  
     input_ids = tokenizer(prompt_template, return_tensors='pt').input_ids.cuda()
     output = model.generate(inputs=input_ids, 
-                            temperature=0.7, 
+                            temperature=temperature, 
                             do_sample=True, 
-                            top_p=0.95, 
-                            top_k=40, 
-                            max_new_tokens=512)
+                            top_p=top_p, 
+                            top_k=top_k, 
+                            max_new_tokens=max_new_tokens)
 
-    return tokenizer.decode(output[0], skip_special_tokens=True)
+    return tokenizer.decode(output[0, input_ids.shape[1]:-1], skip_special_tokens=True)
